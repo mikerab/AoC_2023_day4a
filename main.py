406689ead330a77cbdf6f63d5637b73074b4bf49
mikerab <mrabatin@gmail.com>
2024-12-04 08:02:38 -0500

    Removed an extra loop.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import time
 
-# This is part one... part two will build on top.
 doc = open("input.txt", "rt")
 
-total_points = 0
+original_cards = []
+copied_cards = []
+
+start_time = time.time()
 
 for line in doc:
     line = " ".join(line.split())
-    print(line)
+    # print(line)
     card_name = line.split(":")
+    original_cards.append(card_name[0])
     # print(card_name)
     all_numbers = card_name[1]
     # print(all_numbers)
@@ -15,25 +19,30 @@ for line in doc:
     winning_numbers = temp[0].strip().split(" ")
     card_numbers = temp[1].strip().split(" ")
 
-    # print(f"winning numbers: {winning_numbers}")
-    # print(f"my numbers: {card_numbers}")
-
     numbers_won = []
-    card_points = 0
+    card_copies = 0
 
     for num in card_numbers:
         if num in winning_numbers:
             numbers_won.append(num)
-            if card_points == 0:
-                card_points = 1
-            else:
-                card_points = card_points * 2
+            card_copies = card_copies + 1
 
-    total_points = total_points + card_points
 
-    print(f"{card_name[0]} has {len(numbers_won)} match(es): {numbers_won} and has {card_points} points.")
+    # print(f"{card_name[0]} has {len(numbers_won)} match(es): {numbers_won} and gets {card_copies} additional cards.")
 
-print(f"The pile of cards has {total_points} total points!!")
+    # award additional cards from the original card
+    temp = card_name[0].split(" ")
+    orig_card_number = int(temp[1])
+    count_of_copies = copied_cards.count(card_name[0])
+    for outer in range(1, 2 + count_of_copies):
+        for i in range(orig_card_number + 1, orig_card_number + 1 + card_copies):
+            copied_cards.append("Card " + str(i))
+
+# print(f"The original list of cards is {original_cards}")
+# print(f"The copied list of cards is {copied_cards}")
+
+elapsed_time = time.time() - start_time
+print(f"The total number of cards is {len(original_cards) + len(copied_cards)} (Elapsed Time: {elapsed_time})")
 
 
 doc.close()
